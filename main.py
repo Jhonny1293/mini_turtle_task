a8bb66084d1abc53634edeae88fe7d9425d25e41
Jhonatan Restrepo Montoya <124320588+Jhonny1293@users.noreply.github.com>
2025-12-14 20:26:17 -0500

    script para dibujar una escalera

---
+++ main.py
@@ -0,0 +1,16 @@
+from mini_turtle import mover_derecha, mover_abajo, reiniciar
+
+# Dibujar una escalera
+mover_derecha(5)
+mover_abajo(3)
+
+mover_derecha(4)
+mover_abajo(2)
+
+# Reiniciar posición
+print("Reiniciando...")
+reiniciar()
+
+# Dibujar algo nuevo
+mover_derecha(8)
+mover_abajo(4)
